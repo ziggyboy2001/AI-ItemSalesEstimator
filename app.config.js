7597d3df9afebc2c7be58483ca42b01acd46a1c1
brent.purks <brent.purks@telrite.com>
2025-05-30 16:25:36 -0400

    fixed dependency issues

--- app.config.js
+++ app.config.js
@@ -2,10 +2,49 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    // ...other config...
+    name: "BidPeek",
+    slug: "ebay-resale-estimator",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "bidpeek",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#000000"
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      bundleIdentifier: "com.bidpeek.app",
+      supportsTablet: true
+    },
+    android: {
+      package: "com.bidpeek.app",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#000000"
+      }
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router"
+    ],
+    experiments: {
+      typedRoutes: true
+    },
     extra: {
       OPENAI_API_KEY: process.env.EXPO_PUBLIC_OPENAI_API_KEY,
       PERPLEXITY_API_KEY: process.env.EXPO_PUBLIC_PERPLEXITY_API_KEY,
+      "eas": {
+        "projectId": "6ce7af29-946f-4907-ab49-0afa92e97b72"
+      }
     },
   },
 };
